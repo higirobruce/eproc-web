da26ddaac15523ead78bdaf60a526ad709ddc47f
higirobruce <higirobru@gmail.com>
2023-04-06 14:57:42 +0200

    Update loginText.js

--- components/common/loginText.js
+++ components/common/loginText.js
@@ -4,25 +4,20 @@ import React from "react";
 
 export default function LoginText() {
   return (
-    <div className="col-span-2 hidden md:flex flex-col px-10 pt-10 items-start">
-      <Image alt="" src="/icons/white icon.png" width={236} height={220} />
-      <div className="grid grid-cols-2 px-10 content-center">
-        <div className="self-center flex flex-col">
-          {/* <Typography.Title level={2}>
-            <p className="text-white">Welcome</p>
-          </Typography.Title> */}
-          <Typography.Title level={1}>
-            Irembo Procure
-          </Typography.Title>
-          <div className="text-white font-mono text-sm">
-            A tool that aims to simplify the procurement process for suppliers
-            looking to work with Irembo.
-          </div>
-        </div>
-
-        <div className="opacity-80 absolute left-1/3 w-96 h-96">
-          <Image alt="" src="/login.svg" fill />
-        </div>
+    <div className="col-span-2 hidden md:flex flex-col px-10 items-center justify-center">
+      <div className="opacity-80">
+        <Image alt="" src="/login.svg" width={400} height={400} />
+      </div>
+      <Typography.Title level={2}>
+        <div className="text-white">Welcome to Irembo Procure</div>
+      </Typography.Title>
+      <div className="text-white font-mono text-sm">
+        A tool that aims to simplify the procurement process for suppliers
+        looking to work with Irembo.
+      </div>
+      {/* <Image alt="" src="/icons/white icon.png" width={236} height={220} /> */}
+      <div className="">
+        <div className="self-center flex flex-col"></div>
       </div>
     </div>
   );
